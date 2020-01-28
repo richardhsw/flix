6074212859d4af57645047598831fb20be814b33
Richard Hsu <shengwh1@uci.edu>
2020-01-27 19:54:56 -0800

    superheroes collection view

---
+++ flix/Views/MoviesCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MoviesCollectionViewCell.swift
+//  flix
+//
+//  Created by Richard Hsu on 2020/1/25.
+//  Copyright © 2020 Richard Hsu. All rights reserved.
+//
+
+import UIKit
+
+class MoviesCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
